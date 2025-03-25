11a547466a01121a06f87f6d38d83cb34b239896
SWAMI <swamikumar5610@gmail.com>
2025-03-26 00:14:57 +0530

    Day 14

---
+++ Lecture 13/index.js
@@ -0,0 +1,36 @@
+let age=13;
+if(age>=18)
+{
+    console.log("Eligible");
+}
+else
+{
+    console.log("Not eligible");
+}
+
+
+// Multiple condition switch
+switch(new Date().getDay())
+{
+    case 0:
+        console.log("SUNDAY");
+        break;
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURSDAY");
+        break;
+    case 5:
+        console.log("FRIDAY");
+        break;
+    case 6:
+        console.log("SATURDAY");
+        break;
+}
